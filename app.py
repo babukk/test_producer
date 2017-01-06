222a1e7dbfbe518c36e9fd5ab131005757a67b73
kostamaster <konstantin.bakunov@gmail.com>
2017-01-07 01:16:57 +0300

    worker added

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 
 from flask import Flask, request, abort
 from flask_restful import  reqparse, abort, Api, Resource
+from rq import Queue
+from rq.job import Job
+
+from worker import conn
 
 app = Flask(__name__)
 api = Api(app)
+rqueue = Queue(connection=conn)
+
+# ----------------------------------------------------------------------------------------------------------------------
+def xxx(url):
+    print '--------------------------------> url = ' + url
+    pass
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 class CreateJobByURL(Resource):
@@ -11,7 +22,12 @@ class CreateJobByURL(Resource):
          args = parser.parse_args()
          print repr(args)
          print 'URL = ' + args['url']
-         return {'URL': args['url']}
+         job = rqueue.enqueue_call(
+            func=xxx, args=(args['url'],), result_ttl=5000
+         )
+         print('job = ' + job.get_id())
+
+         return {'URL': args['url'], 'JOB': job.get_id()}
 
 
 # ----------------------------------------------------------------------------------------------------------------------
