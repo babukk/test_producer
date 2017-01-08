f576c5ae24147e007d1ca3016bbcc197ba401313
kostamaster <konstantin.bakunov@gmail.com>
2017-01-08 20:29:31 +0300

    renamed/moved: _tests/test1.sh -> tests/test1.sh

--- tests/test1.sh
+++ tests/test1.sh
@@ -16,4 +16,3 @@ curl -H "Content-Type: application/json" \
 curl -H "Content-Type: application/json" \
     http://127.0.0.1:5000/create_job \
     -X POST -d '{"url":"http://www.boohoo.com/restofworld/accessories/beauty/icat/beauty#esp_pg=4"}'
-
